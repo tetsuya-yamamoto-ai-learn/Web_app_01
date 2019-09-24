9be78a9012516ef44d8c7de601ad6b3dfaaaf865
tetsuya-yamamoto-ai-learn <tetsuya.yamamoto.ai.study@gmail.ocm>
2019-09-24 16:09:33 +0900

    おみくじアプリを実装
    - random関数を用いて運勢をランダムに生成

--- app01.py
+++ app01.py
@@ -1,3 +1,6 @@
+from random import choice
+from random import randint
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -18,6 +21,19 @@ def goodbye():
 @app.route('/add/<a>/<b>')
 def add(a, b):
     return str(int(a) + int(b))
+
+
+@app.route('/omikuji')
+def omikuji():  # ←ViewFunction
+    運 = ['大吉', '吉', '凶']
+    return '今日の運勢は...' + 運[randint(0, len(運))]
+
+
+@app.route('/omikuji_ans')
+def omikuji():  # ←ViewFunction
+    運 = ['大吉', '吉', '凶']
+    result = choice(運)
+    return '今日の運勢は...' + result
 
 
 if __name__ == '__main__':
